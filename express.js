5f2f14d86d6d4070d82abbe9a6a7658161810923
pracvp <66632107+pracvp@users.noreply.github.com>
2021-01-05 00:16:17 +0530

    Update express.js

--- express.js
+++ express.js
@@ -10,4 +10,4 @@ app.use(express.static(__dirname + '/public'));
     }); 
 
 
-app.listen( 3000, () => console.log('Gator app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Gator app listening on port 3000!'));
